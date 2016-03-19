c3f3cb632590849f40eb46ade6f56ec1dafc58b9
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-03-19 14:48:30 +1000

    null

--- troll/models.py
+++ troll/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Session(models.Model):
-    key = models.CharField(max_length=40)
+    key = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
