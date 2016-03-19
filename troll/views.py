005ed20d065a4eb9d90265f2b5b8ea00f34adfcb
Kendrick Tan <kendricktan0814@gmail.com>
2016-03-19 18:12:47 +1000

    added vote

--- troll/views.py
+++ troll/views.py
@@ -46,8 +46,36 @@ def add(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Your troll has been submited for approval")
-            return HttpResponseRedirect(reverse('troll:index'))
+            return HttpResponseRedirect(reverse('troll:vote'))
     else:
         form = IdeaForm(sessionkey=request.session.session_key)
     context["form"] = form
     return render(request, 'add.html', context)
+
+def vote(request):
+    context = {}
+
+    if not request.session.get('has_session'):
+        request.session['has_session'] = True
+
+    if request.method == 'POST':
+        idea_id = request.POST['idea_id']
+        form = LikeForm(request.session.session_key, idea_id, request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({})
+
+    context['ideas_tuple'] = []
+    ideas = Idea.objects.filter(valid=True)
+    for idea in ideas:
+        session, is_new = Session.objects.get_or_create(key=request.session.session_key)
+        likes = Like.objects.filter(idea=idea, session=session)
+        if not is_new and len(likes) == 1 and likes[0].liked:
+            already_liked = True
+        else:
+            already_liked = False
+        context['ideas_tuple'].append((idea, already_liked))
+
+    likeform = LikeForm(sessionkey=request.session.session_key)
+    context['likeform'] = likeform
+    return render(request, 'vote.html', context)
